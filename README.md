39981624a15271e24285bcec69e40e251432d400
Param Pandey <149531051+Parampandey@users.noreply.github.com>
2026-02-14 13:44:00 +0530

    Add files via upload
    Initial project upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Smart Expense Tracker (Python)
+
+A Python-based expense management application to record, categorize, and analyze daily financial spending using CSV data storage.
+
+## Features
+- Add daily expenses
+- Category-wise analysis
+- Monthly spending summary
+- CSV file-based persistence
+- Modular code design
+
+## Tech Stack
+Python, CSV, File Handling
+
+## How to Run
+python expense_tracker.py
---
+++ expense_tracker.py
@@ -0,0 +1,85 @@
+import csv
+import csv
+from datetime import datetime
+
+FILE_NAME = "expenses.csv"
+
+
+def add_expense():
+    date = input("Enter date (YYYY-MM-DD): ")
+    category = input("Enter category: ")
+    amount = float(input("Enter amount: "))
+    description = input("Enter description: ")
+
+    with open(FILE_NAME, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([date, category, amount, description])
+
+    print("Expense added successfully!\n")
+
+
+def view_expenses():
+    with open(FILE_NAME, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+
+
+def monthly_summary():
+    month = input("Enter month (YYYY-MM): ")
+    total = 0
+
+    with open(FILE_NAME, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row["Date"].startswith(month):
+                total += float(row["Amount"])
+
+    print(f"Total spending in {month}: {total}\n")
+
+
+def category_analysis():
+    categories = {}
+
+    with open(FILE_NAME, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            category = row["Category"]
+            amount = float(row["Amount"])
+
+            if category in categories:
+                categories[category] += amount
+            else:
+                categories[category] = amount
+
+    print("Category-wise Spending:")
+    for cat, amt in categories.items():
+        print(cat, ":", amt)
+
+
+def main():
+    while True:
+        print("\n1. Add Expense")
+        print("2. View Expenses")
+        print("3. Monthly Summary")
+        print("4. Category Analysis")
+        print("5. Exit")
+
+        choice = input("Choose option: ")
+
+        if choice == "1":
+            add_expense()
+        elif choice == "2":
+            view_expenses()
+        elif choice == "3":
+            monthly_summary()
+        elif choice == "4":
+            category_analysis()
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice!")
+
+
+if __name__ == "__main__":
+    main()
